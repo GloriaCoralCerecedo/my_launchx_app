37026d4b89da4f1df02cbf6e3c939e89a4b9a5d2
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2022-04-29 02:09:49 -0500

    Probando pruebas

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -10,7 +10,7 @@ describe("Esto es una suite de pruebas", () => {
   describe("Clase  Mission Commander", () => {
     test('1) Crear un objeto a mission commander', () => {
         const myMissionCommander = new MissionCommander("Woopa")
-        expect(myMissionCommander.name).toBe(a);
+        expect(myMissionCommander.name).toBe("Woopa");
     });
 })
   

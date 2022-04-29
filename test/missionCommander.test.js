ae8d22efd77d751a412b216d4a3f62f687639f49
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2022-04-29 02:07:06 -0500

    Agregando Prueba a clase Mission Commander

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -7,7 +7,7 @@ describe("Esto es una suite de pruebas", () => {
     });
   })
 
-  describe("Clase mission commander", () => {
+  describe("Clase  Mission Commander", () => {
     test('1) Crear un objeto a mission commander', () => {
         const myMissionCommander = new MissionCommander("Woopa")
         expect(myMissionCommander.name).toBe("Woopa");

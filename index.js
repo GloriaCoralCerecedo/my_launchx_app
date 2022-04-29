250d72df86ea26827c16ab0a6afccc9904e27f8c
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2022-04-29 01:44:36 -0500

    Agregando objetos a la clase MissionCommander en  index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const MissionCommander = require('./app/missionCommander');
 const gloria= new MissionCommander("Gloria")
+const carlo = new MissionCommander ("Carlo");
+const nicole = new MissionCommander ("Nicole");
 console.log(gloria.name)
+console.log(carlo.name)
+console.log(nicole.name)
